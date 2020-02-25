a66d5ef8bac96dd893524a12a70449593e5b64bf
javi040898 <57791218+javi040898@users.noreply.github.com>
2020-02-25 13:17:01 +0100

    falta 8, 21 y conclusiones de los finales

--- LABORATORIO/PECL1/ConsultasIndice.sql
+++ LABORATORIO/PECL1/ConsultasIndice.sql
@@ -83,4 +83,97 @@ create table "particion9" partition of "MiTabla3" for values with (modulus 10, r
 
 select count(*) from "particion9";
 
+select oid,relname,relpages from pg_class where oid=16643 or oid=16649 or oid=16655 or oid=16661 or oid=16667 or oid=16673 or
+oid=16679 or oid=16685 or oid=16691 or oid=16697;
 
+create index "MiTablaBTree" on "MiTabla" using btree(puntos); 
+
+create index "HashPuntosMiTabla" on "MiTabla" using hash(puntos);
+create index "HashIdMiTabla" on "MiTabla" using hash(id_cliente);
+
+select * from "MiTabla";
+
+select * from pg_class where oid=16719 or oid=16718 or oid=16717;
+
+
+create extension pg_stat_statements;
+
+select * from pg_stat_statements;
+
+select * from pg_settings where name='config_file'
+select * from pg_stat_database;
+
+
+
+
+select * from "MiTabla" where "id_cliente"=8101000;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+select * from "MiTabla" where "id_cliente"<30000;
+
+select * from "MiTabla" where "id_cliente">8000 and "id_cliente"<10000;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+select * from "MiTabla" where "id_cliente">34500 or "id_cliente"<30204000;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+select * from "MiTabla" where not("id_cliente"=3450000);
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+
+select * from "MiTabla" where "nombre"='nombre3456789';
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+select * from "MiTabla" where "puntos"=650;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+select * from "MiTabla" where "puntos"<200;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+
+select * from "MiTabla" where "puntos">30000;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+select * from "MiTabla" where "puntos"=230 and "id_cliente"=90000;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+select * from "MiTabla" where "puntos"=230 or "id_cliente"=90000;
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+
+
+create index "IndiceMulti" on "MiTabla" (puntos,nombre);
+
+
+select * from "MiTabla" where "puntos"=200 or "nombre"='nombre3456789';
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+select pg_stat_reset();
+
+select pg_stat_reset();
+select * from "MiTabla" where "puntos"=200 and "nombre"='nombre3456789';
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+select pg_stat_reset();
+select * from "MiTabla" where "id_cliente"=6000 or "nombre"='nombre3456789';
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+select pg_stat_reset();
+select * from "MiTabla" where "id_cliente"=6000 and "nombre"='nombre3456789';
+select * from pg_statio_all_tables where schemaname = 'public' ;
+
+
+
+
+
+select pg_stat_reset();
+select * from "MiTabla3" where "puntos"=800;
+select * from pg_statio_all_tables where schemaname = 'public' ;
