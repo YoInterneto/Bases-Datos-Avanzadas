1a0fdb6be429420aa0e806d0c2f0fb2e0f0ac82b
javi040898 <57791218+javi040898@users.noreply.github.com>
2020-03-10 10:01:43 +0100

    avanzando

--- LABORATORIO/PECL1/ScriptPecl2.sql
+++ LABORATORIO/PECL1/ScriptPecl2.sql
@@ -21,3 +21,34 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.trabaja_proyectos
     OWNER to postgres;
+	
+	
+	
+	
+select distinct nombre from "empleados";	
+
+
+select * from pg_stats where tablename='proyectos' or tablename='empleados' or tablename='trabaja_proyectos';
+
+
+create tablespace bloques location 'C:\Users\Javier\Desktop\INFORMATICA';
+
+alter tablespace bloques set (seq_page_cost=1, random_page_cost=1 );
+
+alter tablespace bloques1 set( seq_page_cost(1));
+
+random_page_cost (1);
+
+cpu_tuple_cost (0);
+cpu_index_tuple_cost (0)
+cpu_operator_cost (0);
+parallel_setup_cost (0);
+parallel_tuple_cost (0);
+min_parallel_table_scan_size (0);
+min_parallel_index_scan_size (0);
+effective_cache_size (0);
+jit_above_cost (0);
+jit_inline_above_cost (0);
+jit_optimize_above_cost (0);
+
+
